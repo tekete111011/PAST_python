a0a3be45e14cec44fb8a17a2b0c070393b8a702c
Rye <ryebreadoisii@gmail.com>
2021-04-18 22:51:08 +0900

    yeah

--- chap5/6_3_1.py
+++ chap5/6_3_1.py
@@ -5,3 +5,28 @@ sy, sx = map(int, input().split() )
 gy, gx = map(int, input().split() )
 S = [ input() for i in range(R) ]
 
+sy -= 1
+sx -= 1
+gy -= 1
+gx -= 1
+
+dist = [ [-1]*C for i in range(R) ]
+
+Q = deque()
+Q.append([sy, sx])
+dist[sy][sx] = 0
+
+while len(Q) > 0:
+  i, j = Q.popleft()
+
+  for i2, j2 in [ [i,j-1], [i, j+1], [i-1,j], [i+1,j] ]:
+    if not (0<=i2<R and 0<=j2<C):
+      continue
+    if S[i][j] == "#":
+      continue
+    if dist[i2][j2] == -1:
+      dist[i2][j2] = dist[i][j] + 1
+      Q.append([i2, j2])
+
+print(dist[gy][gx])
+print(dist)
